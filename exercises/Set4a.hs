ac06b6167c13237c89c1267933cabf11879d3a93
Jamb000h <8905786+Jamb000h@users.noreply.github.com>
2021-11-05 21:23:09 +0200

    Set4A

--- exercises/Set4a.hs
+++ exercises/Set4a.hs
@@ -35,7 +35,9 @@ import Data.Array
 -- you remove the Eq a => constraint from the type!
 
 allEqual :: Eq a => [a] -> Bool
-allEqual xs = todo
+allEqual [] = True
+allEqual [x] = True
+allEqual (x:y:xs) = x == y && allEqual (y:xs)
 
 ------------------------------------------------------------------------------
 -- Ex 2: implement the function distinct which returns True if all
@@ -49,8 +51,13 @@ allEqual xs = todo
 --   distinct [1,1,2] ==> False
 --   distinct [1,2] ==> True
 
-distinct :: Eq a => [a] -> Bool
-distinct = todo
+distinct :: (Eq a, Ord a) => [a] -> Bool
+distinct xs = distinct' (sort xs)
+
+distinct' :: Ord a => [a] -> Bool
+distinct' [] = True
+distinct' [x] = True
+distinct' (x:y:xs) = x < y && distinct' (y:xs)
 
 ------------------------------------------------------------------------------
 -- Ex 3: implement the function middle that returns the middle value
@@ -63,7 +70,8 @@ distinct = todo
 --   middle 'b' 'a' 'c'  ==> 'b'
 --   middle 1 7 3        ==> 3
 
-middle = todo
+middle :: Ord a => a -> a -> a -> a
+middle x y z = sort [x,y,z] !! 1
 
 ------------------------------------------------------------------------------
 -- Ex 4: return the range of an input list, that is, the difference
@@ -78,8 +86,13 @@ middle = todo
 --   rangeOf [4,2,1,3]          ==> 3
 --   rangeOf [1.5,1.0,1.1,1.2]  ==> 0.5
 
-rangeOf :: [a] -> a
-rangeOf = todo
+rangeOf :: (Num a, Ord a) => [a] -> a
+rangeOf (x:y:xs) = let sortedA = sort (x:y:xs)
+                       first = head sortedA
+                       last = sortedA !! (length sortedA - 1)
+                in abs (first - last)
+rangeOf _ = 0
+
 
 ------------------------------------------------------------------------------
 -- Ex 5: given a (non-empty) list of (non-empty) lists, return the longest
@@ -97,7 +110,14 @@ rangeOf = todo
 --   longest [[1,2,3],[4,5],[6]] ==> [1,2,3]
 --   longest ["bcd","def","ab"] ==> "bcd"
 
-longest = todo
+longest :: Ord a => [[a]] -> [a]
+longest xs = let sortedListOfLists = sortByLength xs
+                 filtered = filter (\x -> length x == length (sortedListOfLists !! (length sortedListOfLists - 1)))  sortedListOfLists
+                 smallestValue = minimum filtered
+              in smallestValue
+
+sortByLength :: [[a]] -> [[a]]
+sortByLength = sortOn length
 
 ------------------------------------------------------------------------------
 -- Ex 6: Implement the function incrementKey, that takes a list of
@@ -113,8 +133,8 @@ longest = todo
 --   incrementKey True [(True,1),(False,3),(True,4)] ==> [(True,2),(False,3),(True,5)]
 --   incrementKey 'a' [('a',3.4)] ==> [('a',4.4)]
 
-incrementKey :: k -> [(k,v)] -> [(k,v)]
-incrementKey = todo
+incrementKey :: (Eq k, Num v) => k -> [(k,v)] -> [(k,v)]
+incrementKey k = map (\(kx, vx) -> if kx == k then (kx, vx + 1) else (kx, vx))
 
 ------------------------------------------------------------------------------
 -- Ex 7: compute the average of a list of values of the Fractional
@@ -129,7 +149,7 @@ incrementKey = todo
 -- length to a Fractional
 
 average :: Fractional a => [a] -> a
-average xs = todo
+average xs = sum xs / fromIntegral (length xs)
 
 ------------------------------------------------------------------------------
 -- Ex 8: given a map from player name to score and two players, return
@@ -148,7 +168,9 @@ average xs = todo
 --     ==> "Lisa"
 
 winner :: Map.Map String Int -> String -> String -> String
-winner scores player1 player2 = todo
+winner scores player1 player2 = let p1 = Map.findWithDefault 0 player1 scores
+                                    p2 = Map.findWithDefault 0 player2 scores
+                                in if p1 >= p2 then player1 else player2
 
 ------------------------------------------------------------------------------
 -- Ex 9: compute how many times each value in the list occurs. Return
@@ -163,7 +185,12 @@ winner scores player1 player2 = todo
 --     ==> Map.fromList [(False,3),(True,1)]
 
 freqs :: (Eq a, Ord a) => [a] -> Map.Map a Int
-freqs xs = todo
+freqs xs = freqs' xs Map.empty
+
+freqs' :: (Ord a) => [a] -> Map.Map a Int -> Map.Map a Int
+freqs' [] m = m
+freqs' (x:xs) m = let c = Map.findWithDefault 0 x m
+                  in freqs' xs (Map.insert x (c + 1) m)
 
 ------------------------------------------------------------------------------
 -- Ex 10: recall the withdraw example from the course material. Write a
@@ -191,7 +218,15 @@ freqs xs = todo
 --     ==> fromList [("Bob",100),("Mike",50)]
 
 transfer :: String -> String -> Int -> Map.Map String Int -> Map.Map String Int
-transfer from to amount bank = todo
+transfer from to amount bank
+    | amount <= 0 = bank
+    | Map.notMember from bank = bank
+    | Map.notMember to bank = bank
+    | otherwise = case Map.lookup from bank of
+                    Nothing  -> bank
+                    Just sum -> if sum < amount
+                                then bank
+                                else Map.adjust (+ amount) to (Map.adjust (\x -> x - amount) from bank)
 
 ------------------------------------------------------------------------------
 -- Ex 11: given an Array and two indices, swap the elements in the indices.
@@ -201,7 +236,7 @@ transfer from to amount bank = todo
 --         ==> array (1,4) [(1,"one"),(2,"three"),(3,"two"),(4,"four")]
 
 swap :: Ix i => i -> i -> Array i a -> Array i a
-swap i j arr = todo
+swap i j arr = arr // [(i, arr ! j), (j, arr ! i)]
 
 ------------------------------------------------------------------------------
 -- Ex 12: given an Array, find the index of the largest element. You
@@ -212,4 +247,12 @@ swap i j arr = todo
 -- Hint: check out Data.Array.indices or Data.Array.assocs
 
 maxIndex :: (Ix i, Ord a) => Array i a -> i
-maxIndex = todo
+maxIndex arr = let kvA = assocs arr
+               in maxIndex' (fst (head kvA)) (snd (head kvA)) kvA
+
+maxIndex' :: Ord a => i -> a -> [(i, a)] -> i
+maxIndex' mI _ [] = mI
+maxIndex' mI max ((i, e):xs) =
+    if e > max
+    then maxIndex' i e xs
+    else maxIndex' mI max xs
